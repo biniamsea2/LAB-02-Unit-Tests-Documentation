e9e52420b56c8417e20fe59d65c75d29b2bb0c34
Biniam <biniamt23@yahoo.com>
2019-10-16 09:47:50 -0700

    gitignore, and screenshot folder added

---
+++ LAB02UnitTests/LAB02UnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB02UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
